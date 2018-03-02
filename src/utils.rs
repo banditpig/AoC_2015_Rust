9b7b8da13c12d148accf973bafb111fdae01db4e
mike-houghton <mike.houghton@feefo.com>
2018-03-02 20:49:14 +0000

    added func for checking if vec has dups.

--- src/utils.rs
+++ src/utils.rs
@@ -8,3 +8,12 @@ pub fn read_file(filename: String) -> String {
 
     contents
 }
+pub fn has_duplicates<T: Eq + Ord + Clone>(v: &Vec<T>) -> bool {
+    if v.len() == 0 { return false; }
+
+    let (head, tail) = v.split_at(1);
+    if tail.contains(&head[0]) {
+        return true;
+    }
+    has_duplicates(&tail.to_vec())
+}

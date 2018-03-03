cffcb7feea021acb078ea06dfb35a23b058e3d3f
mike-houghton <mike.houghton@feefo.com>
2018-03-03 19:08:54 +0000

    latest

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 //use std::fs::File;
 //use std::io::prelude::*;
 //use std::cmp;
+use std::thread;
+
 extern crate crypto;
 
 mod day1;
 mod day2;
 mod day3;
 mod day4;
-mod file_utils;
+mod day5;
+mod day6;
+mod utils;
+
 fn main() {
-    day1::part1_part2();
-    day2::part1_part2();
-    day3::part1_part2();
-    day4::part1_part2();
+   day1::part1_part2();
+   day2::part1_part2();
+   day3::part1_part2();
+   day4::part1_part2();
+   day5::part1_part2();
+   day6::part1_part2();
+
+
+
+
 }
